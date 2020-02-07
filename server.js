6fe9b172de92a8408d48192e6c58634b73a368e2
Arvind <arvind@wizni.com>
2020-02-07 18:34:18 +0530

    ++ Fix Name

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/'+'biz2credit'));
+app.use(express.static(__dirname + '/dist/'+'Biz2credit'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/biz2credit/'+index.html));
+  res.sendFile(path.join(__dirname+'/Biz2credit/'+index.html));
 });
 
 // Start the app by listening on the default Heroku port
